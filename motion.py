03ba170e067fad9b97814db94aebd6fce2481ac7
chinmay1 <cparikh7@gmail.com>
2013-07-14 20:23:12 +0530

    Update motion.py
    base now average of new and base.

--- motion.py
+++ motion.py
@@ -4,17 +4,24 @@ import time
 def diffImg0(t0,t1):
     d = cv2.absdiff(t0,t1)
     return cv2.countNonZero(d)
+
+
 cam = cv2.VideoCapture(0)
 t = cv2.cvtColor(cam.read()[1], cv2.COLOR_BGR2GRAY)
 t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_BGR2GRAY)
-prev = 1
+
+base = diffImg0(t,t_plus)
+
+
 while True:
+
   t = t_plus
   t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_BGR2GRAY)
-  base = diffImg0(t,t_plus)
-  back = prev-(prev/10)
-  front =  prev+(prev/10)
-  if((base > front ) or (base < back)):
-      cv2.imwrite(strftime("%Y.%m.%d.%H.%M.%S", gmtime())+".png",t_plus)
+
+  new = diffImg0(t,t_plus)
+  back = base - (base *(37.0/100.0))
+  front = base + (base *(37.0/100.0))  
+  base = (new+base)/2
+  if((new > front ) or (new < back)):
       time.sleep(1)
-      prev = base
+      cv2.imwrite(strftime("%Y.%m.%d.%H.%M.%S", gmtime())+".png",t_plus)
